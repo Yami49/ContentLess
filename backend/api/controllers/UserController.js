e065d34b4ec507c5c502332d1ab369bdddaa4d29
Georgios Gerontidis <georgiosgerontidis1@gmail.com>
2024-12-16 16:36:56 +0100

    backend und frontend hinzugefügt und verbunden

---
+++ backend/api/controllers/UserController.js
@@ -0,0 +1,17 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+    getAllUsers: async function (req, res) {
+        return res.json([
+          { id: 1, name: "Max Mustermann" },
+          { id: 2, name: "Erika Musterfrau" },
+          { id: 3, name:"tes test"},
+        ]);
+      },
+};
+
---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>User List</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  mounted() {
+    this.fetchUsers();
+  },
+  methods: {
+    async fetchUsers() {
+      try {
+        const response = await axios.get('http://localhost:1337/api/users');
+        this.users = response.data;
+      } catch (error) {
+        console.error('Fehler beim Abrufen der Benutzer:', error);
+      }
+    },
+  },
+};
+</script>
